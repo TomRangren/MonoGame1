459d775c10c4ac1311b80393eed931fc3d542bca
TomRangren <54435262+TomRangren@users.noreply.github.com>
2020-03-03 13:57:04 +0100

    Update Fiende.cs

--- Fiende.cs
+++ Fiende.cs
@@ -1,20 +1,32 @@
- Vector2 position;
-        Texture2D texture;
-        Vector2 center;
-        float angle = 0;
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-        public Fiende(Texture2D texture, Vector2 position)
+namespace Template
+{
+    class Fiende
+    {
+        Vector2 position;
+        Texture2D texture;
+   
+
+        public Fiende(Texture2D texture)
         {
             this.texture = texture;
-            this.center = position;
         }
+        public Vector2 FiendePos { set { position = value; } }
         public void Update()
         {
-            angle+= .1f;
-            position.X = center.X + (float)Math.Sin(angle)* 700;
-            position.Y = center.Y + (float)Math.Cos(angle);
+            position.X -= 10;
         }
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, new Rectangle(position.ToPoint(), new Point(200, 200)), Color.White);
         }
+    }
+}
